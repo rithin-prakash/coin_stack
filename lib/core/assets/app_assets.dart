cc0b1a156c20e6ffa0f96e0709246bf1409fe9e0
Rithin Prakash A <rithinprakash@gmail.com>
2025-03-15 23:10:41 +0530

    intro assets

---
+++ lib/core/assets/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const svgPath = 'assets/svg';
+
+  //svg
+  static const introTrust = '$svgPath/trust.svg';
+  static const introMoneyAbroad = '$svgPath/send_money_abroad.svg';
+  static const introReceiveMoney = '$svgPath/receive_money.svg';
+}
