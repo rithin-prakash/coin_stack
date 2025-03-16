c229bb1459118210a0445ed2a763cf83f8343ab7
Rithin Prakash A <rithinprakash@gmail.com>
2025-03-16 16:48:05 +0530

    button theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,8 +1,18 @@
 import 'package:coin_stack/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
     colorScheme: ColorScheme.light(primary: AppColors.primary),
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        fixedSize: Size(70, 50),
+        textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        foregroundColor: Colors.white,
+      ),
+    ),
   );
 }
