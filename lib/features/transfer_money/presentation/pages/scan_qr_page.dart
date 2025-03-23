3ea374912f0412cff3dde1fc016074356e921334
Rithin Prakash A <rithinprakash@gmail.com>
2025-03-23 12:58:26 +0530

    scan qr code page

---
+++ lib/features/transfer_money/presentation/pages/scan_qr_page.dart
@@ -0,0 +1,178 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:torch_light/torch_light.dart';
+
+class ScanQrPage extends StatefulWidget {
+  const ScanQrPage({super.key});
+
+  @override
+  State<ScanQrPage> createState() => _ScanQrPageState();
+}
+
+class _ScanQrPageState extends State<ScanQrPage> with WidgetsBindingObserver {
+  final MobileScannerController controller = MobileScannerController();
+
+  StreamSubscription<Object?>? _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start listening to lifecycle changes.
+    WidgetsBinding.instance.addObserver(this);
+
+    // Start listening to the barcode events.
+    _subscription = controller.barcodes.listen(_handleBarcode);
+
+    // Finally, start the scanner itself.
+    unawaited(controller.start());
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (!controller.value.hasCameraPermission) {
+      return;
+    }
+
+    switch (state) {
+      case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+      case AppLifecycleState.paused:
+        return;
+      case AppLifecycleState.resumed:
+        _subscription = controller.barcodes.listen(_handleBarcode);
+        unawaited(controller.start());
+      case AppLifecycleState.inactive:
+        unawaited(_subscription?.cancel());
+        _subscription = null;
+        unawaited(controller.stop());
+    }
+  }
+
+  void _handleBarcode(BarcodeCapture event) {
+    // print(event);
+  }
+
+  @override
+  Future<void> dispose() async {
+    WidgetsBinding.instance.removeObserver(this);
+    unawaited(_subscription?.cancel());
+    _subscription = null;
+    super.dispose();
+    await controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // late final scanWindow = Rect.fromCenter(
+    //   center: MediaQuery.sizeOf(context).center(const Offset(0, -100)),
+    //   width: 300,
+    //   height: 200,
+    // );
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(backgroundColor: Colors.transparent),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // MobileScanner(
+          //   scanWindow: scanWindow,
+          //   controller: controller,
+          //   errorBuilder: (context, error, _) {
+          //     return Container();
+          //   },
+          //   fit: BoxFit.contain,
+          // ),
+          Expanded(
+            child: Center(
+              child: QrImageView(
+                data: '1234567890',
+                version: QrVersions.auto,
+                size: 250.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Scan QR code to pay',
+            style: Theme.of(
+              context,
+            ).textTheme.titleLarge?.copyWith(color: Colors.white),
+          ),
+          Text(
+            'Hold the QR inside the frame',
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge?.copyWith(color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          FlashLightContainer(),
+          SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+}
+
+class FlashLightContainer extends StatefulWidget {
+  const FlashLightContainer({super.key});
+
+  @override
+  State<FlashLightContainer> createState() => _FlashLightContainerState();
+}
+
+class _FlashLightContainerState extends State<FlashLightContainer> {
+  bool isTorchAvailable = false;
+  bool flashOn = false;
+
+  flashLightAvailable() async {
+    try {
+      isTorchAvailable = await TorchLight.isTorchAvailable();
+      setState(() {});
+    } on Exception catch (_) {}
+  }
+
+  toggleFlashLight() async {
+    if (!flashOn) {
+      setState(() {
+        flashOn = true;
+      });
+      TorchLight.enableTorch();
+    } else {
+      setState(() {
+        flashOn = false;
+      });
+      TorchLight.disableTorch();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  init() {
+    flashLightAvailable();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !isTorchAvailable
+        ? Container()
+        : IconButton(
+          iconSize: 44,
+          onPressed: () {
+            toggleFlashLight();
+          },
+          icon: Icon(
+            flashOn ? Icons.flash_off : Icons.flash_on,
+            color: Colors.white,
+          ),
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:coin_stack/core/theme/app_theme.dart';
-import 'package:coin_stack/features/transfer_money/presentation/pages/send_money_success_page.dart';
+import 'package:coin_stack/features/transfer_money/presentation/pages/scan_qr_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
-      home: SendMoneySuccessPage(),
+      home: ScanQrPage(),
       // debugShowMaterialGrid: true,
     );
   }
